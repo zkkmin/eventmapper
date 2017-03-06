ee19e42da23586577e0b523068d7630b9195cd72
zkkmin <zkkmin@gmail.com>
2017-03-06 00:22:30 +0000

    Add feature to layers by using json data field

--- maps/static/maps/js/maplayers.js
+++ maps/static/maps/js/maplayers.js
@@ -19,17 +19,22 @@ var maplayersApp = new Vue({
         selectedLayer: 0,
         layer: {
             name: '',
-            level: '',
+            building_level: '',
+            description: '',
+            eventmap: '',
+            json_data: {},
             checked: true,
-            features: [] 
+            features: [],
+            pk: -1,
         }, //layer
         layers: [],
         feature: {
           name: 'Untitled Drawing',
           description: '',
-          drawnLayer: {}
+          data: {}
         }, //feature
         drawnLayer: {}, // {featureid, drawnLayer}
+        json_data: {}, // to hold drawnItems.toGeoJson
     },
     
     computed: {
@@ -82,11 +87,38 @@ var maplayersApp = new Vue({
             var _that = this;
             axios.get('/api/eventmaps/' + this.mapPk + '/mlayers/')
             .then(function (response){
+                
                 var results = response.data.results;
-                _that.layers = [];
+                // console.log(results);
+                // _that.layers = [];
+                
                 for (var i=0; i < results.length; i++ ){
+                    
+                    results[i].features = [];
+                    if (results[i].json_data.features){
+                        results[i].json_data.features.forEach(function( obj) {
+                            results[i].features.push(obj);
+                            console.log(obj);
+                            var temp = L.GeoJSON.geometryToLayer(obj);
+                            // console.log(temp);
+                            
+                            var feature = temp.feature = temp.feature || {}; // Intialize layer.feature
+                            feature.type = feature.type || "Feature"; // Intialize feature.type
+                            feature.id = obj.id;
+                            var props = feature.properties = feature.properties || {}; // Intialize feature.properties
+                            props.name = obj.properties.name;
+                            props.description = obj.properties.description;
+                            props.ownerLayer = obj.properties.ownerLayer;
+                            
+                            drawnItems.addLayer(temp);
+                            
+                        });
+                    }
+                    
                     results[i].checked = false;
+                    
                     _that.layers.push(results[i]);
+                    
                 }
                 
             });
@@ -94,6 +126,7 @@ var maplayersApp = new Vue({
         
         selectLayer: function(index, alayer){
             this.selectedLayer = index;
+            
         },
         
         checkboxChanged: function(changedLayer){
@@ -139,7 +172,56 @@ var maplayersApp = new Vue({
             
         },
         
-        addFeature: function(){},
+        addFeature: function(){
+            // the layer is already saved
+            // just edit the json field with newly created features
+            var layerToEdit = this.layers[this.selectedLayer];
+            
+            var layer = this.drawnLayer,
+            feature = layer.feature = layer.feature || {}; // Intialize layer.feature
+            feature.type = feature.type || "Feature"; // Intialize feature.type
+            feature.id = drawnItems.getLayers().length;
+            var props = feature.properties = feature.properties || {}; // Intialize feature.properties
+            props.name = this.feature.name;
+            props.description = this.feature.description;
+            props.ownerLayer = layerToEdit.pk;
+            drawnItems.addLayer(layer);
+            
+            console.log('add Feature');
+            
+            
+            this.json_data = drawnItems.toGeoJSON();
+            console.log(this.json_data);
+            // Filter only the features of this layer
+            function belongsTo(featureVal){
+                return featureVal.properties.ownerLayer == layerToEdit.pk;
+             }
+             this.json_data.features = this.json_data.features.filter(belongsTo);
+            
+            var _that = this;
+            // https://eventmapper-zkkmin.c9users.io/api/layers/12/
+            axios.patch('/api/layers/' + layerToEdit.pk + '/', 
+                        { json_data: this.json_data }, 
+                        {headers: {"X-CSRFToken": Cookies.get('csrftoken'), 'X-Requested-With': 'XMLHttpRequest'}} 
+            )
+            .then(function(response){
+                console.log(response);
+                
+                layerToEdit.features = [];
+                response.data.json_data.features.forEach(function(obj){
+                    if (obj.properties.ownerLayer == layerToEdit.pk){
+                        layerToEdit.features.push(obj);     
+                    }
+                    
+                });
+                // _that.layers.push(layerToEdit);
+                
+            })
+            .catch(function(error){
+                console.log(error.data)
+            });
+             
+        },
         
         deleteFeature: function(layerIndex, featureIndex) {},
         
